c868388914f3175a1380134b1f3df8668812c544
0kq <61573675+0kq-github@users.noreply.github.com>
2021-07-11 14:26:49 +0900

    Update autoserver.sh

--- autoserver.sh
+++ autoserver.sh
@@ -59,6 +59,8 @@ elif [ $mcver = "1.16.5" ]; then
  curl -L -o server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 elif [ $mcver = "1.17" ]; then
  curl -L -o server.jar https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar
+elif [ $mcver = "1.17.1" ]; then
+ curl -L -o server.jar https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar
 else
  echo "対応していないバージョンです"
  exit
